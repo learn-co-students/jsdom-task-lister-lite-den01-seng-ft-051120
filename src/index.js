10ecfc0ec67a076c7f5cf66bdb2c1f354e6501a7
Zietieflr <lrmcguire93@gmail.com>
2020-06-25 09:51:56 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('input[type="submit"]').addEventListener('click', function(event) {
+    event.preventDefault()
+    const $newTask = document.querySelector('input[type="text"]')
+    addToDo($newTask.value)
+    $newTask.value = ""
+  })
 });
+
+function addToDo(toDo) {
+  const $tasks = document.getElementById('tasks')
+  const $task = document.createElement('li')
+  $task.textContent = toDo
+  $task.addEventListener('dblclick', event => doubleClick(event))
+  remove($task)
+  $tasks.append($task)
+}
+
+function doubleClick(event) {
+  event.stopPropagation() 
+  const originalText = event.target.textContent.slice(0, -1)
+  // replace the li with a field for update 
+  event.target.innerHTML = `
+    <form>
+      <input type='text' value='${originalText}' name='update' />
+    </form>
+  `
+  console.log('event.target = ', event.target)
+  event.target.addEventListener('submit', updateTask)
+}
+
+function updateTask(event) {
+  event.preventDefault()
+  const $formData = new FormData(event.target)
+  // console.log(event.target)
+  // console.log(...$formData)
+  const updatedToDo = $formData.get('update')
+  console.log('parentNode', event.target.parentNode)
+  event.target.parentNode.remove()
+  addToDo(updatedToDo)
+}
+
+function remove($task) {
+  const $remove = document.createElement('button')
+  $remove.textContent = 'x'
+  $remove.addEventListener('click', event => event.path[1].remove())
+  $task.append($remove)
+}
